70766ce65bcfa2a525dfb4e250b405058f9d757a
Toby Kook <toby@kook.work>
2022-09-27 12:56:24 +0100

    Update Groovy script

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo 'Building Docker image'
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASSWORD')]) {
         sh 'docker build -t influous/infx-repo:dm1.1 .'
-        sh "echo $PASSWORD | docker login -u $USER --password-stdin"
+        sh "echo "\$PASSWORD" | docker login -u "\$USER" --password-stdin"
         sh 'docker push influous/infx-repo:dm1.1'
     }
 }
