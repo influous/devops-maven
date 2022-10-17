cb45fb755c0b87918a6ff7dee3a0b21c381578f9
influous <influous@mailbox.org>
2022-10-17 12:41:51 +0200

    Adjust availability zone default

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "subnet_cidr_block" {
   default = "10.0.10.0/24"
 }
 variable "avail_zone" {
-  default = "eu-central-1"
+  default = "eu-central-1a"
 }
 variable "env_prefix" {
   default = "dev"
@@ -17,5 +17,5 @@ variable "instance_type" {
   default = "t2.micro"
 }
 variable "region" {
-  default = "eu-central-1a"
+  default = "eu-central-1"
 }
